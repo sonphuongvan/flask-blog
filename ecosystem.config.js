05f2a8d6f357234995d7719f2b7bc802086ff792
sonpv <sonpv@vmodev.com>
2022-05-19 22:56:29 +0700

    my super secret

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  deploy : {
+    production : {
+      user : 'isla',
+      host : '192.168.1.110',
+      ref  : 'origin/master',
+      repo : 'git@git.vmo.dev:global/vmo-devops-internal/fresher-project/sonpv-project.git',
+      path : '/home/isla/flask_blog',
+      'pre-setup': 'rm -rf /home/isla/flask_blog/*',
+      'post-setup' : 'cd /home/isla/flask_blog/current; python3 -m venv venv; source venv/bin/activate; pip3 install -r requirements.txt; sudo ufw allow 5000; sudo ufw reload; pm2 start ecosystem_run.json --env prod'
+    }
+  }
+};
