da52e48616766c4ceb951369af4dbcb2e6943ee4
sonpv <sonpv@vmodev.com>
2022-05-19 15:44:14 +0700

    fix js->json

--- deploy_pm2.js
+++ deploy_pm2.js
@@ -1,14 +1,13 @@
 module.exports = {
   deploy : {
     production : {
-      key: 'home/isla/.ssh/known_hosts',
       user : 'isla',
       host : '172.16.2.95',
-      ref  : 'origin/main',
+      ref  : 'origin/master',
       repo : 'git@git.vmo.dev:global/vmo-devops-internal/fresher-project/sonpv-project.git',
       path : '/home/isla',
-      'pre-deploy-local': 'pip3 install ',
-      'post-deploy' : '',
+      'pre-deploy-local': '',
+      'post-deploy' : 'touch /tmp/test.txt',
       'pre-setup': ''
     }
   }
