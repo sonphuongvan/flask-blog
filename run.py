aaaf3bfa19ed46685a13948c1590a58e0c77b04f
sonpv <sonpv@vmodev.com>
2022-06-05 14:49:48 +0700

    update: create db

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from flaskblog import create_app
+from flaskblog import create_app, db
 
 app = create_app()
 
 if __name__ == '__main__':
+    db.create_all(app=create_app())
     app.run(debug=True, host='0.0.0.0')
